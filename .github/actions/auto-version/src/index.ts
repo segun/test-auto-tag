f3a132560aa5d94d0d07c569fd6aff4a6d9266f9
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2022-12-29 23:57:26 +0100

    use public action
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- .github/actions/auto-version/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as core from '@actions/core';
-
-const packageDir = core.getInput("PACKAGEJSON_DIR");
-core.debug("Package Json Dir: " + packageDir);
-const tagPrefix = core.getInput("tag-prefix")
-core.debug("Tag Prefix: " + tagPrefix);
-const tagSuffix = core.getInput("tag-suffix")
-core.debug("Tag Suffix: " + tagSuffix);
-console.log(process.env);
